03e3081b25d4e6400927e14932880746fffaca4d
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-25 13:29:51 +0100

    Simultaneous assignments

--- Python tutorials for beginners/data_type.py
+++ Python tutorials for beginners/data_type.py
@@ -3,3 +3,11 @@ pi = 3.14                     # pi is float
 empname = "python is great"   # empname is string
 
 a = b = c = 100 # this statement assign 100 to c, b and a.
+
+
+print("*"*50)
+
+x = 1   # initial value of x is 1
+y = 2   # initial value of y is 2
+
+y, x = x, y # assign y value to x and x value to y
