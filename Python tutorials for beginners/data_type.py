a75adc7f7dfdf31a6ec1d3d4a4b91db38780b18e
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-25 13:57:57 +0100

    nested if

--- Python tutorials for beginners/data_type.py
+++ Python tutorials for beginners/data_type.py
@@ -245,3 +245,22 @@ elif boolean-expression:
    #statements
 else:
    #statements
+
+today = "monday"
+
+if today == "monday":
+   print("this is monday")
+elif today == "tuesday":
+   print("this is tuesday")
+elif today == "wednesday":
+   print("this is wednesday")
+elif today == "thursday":
+   print("this is thursday")
+elif today == "friday":
+   print("this is friday")
+elif today == "saturday":
+   print("this is saturday")
+elif today == "sunday":
+   print("this is sunday")
+else:
+   print("something else")
