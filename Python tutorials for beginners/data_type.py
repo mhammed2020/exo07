a5f8003ddb7d26213f2323acf92e7f49bf0b5c07
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-25 13:40:06 +0100

    list in python

--- Python tutorials for beginners/data_type.py
+++ Python tutorials for beginners/data_type.py
@@ -116,3 +116,11 @@ print(s.find("become"))
 print(s.rfind("o"))
 
 print(s.count("o"))
+
+
+# list 
+
+list1 = list() # Create an empty list
+list2 = list([22, 31, 61]) # Create a list with elements 22, 31, 61
+list3 = list(["tom", "jerry", "spyke"]) # Create a list with strings
+list5 = list("python") # Create a list with characters p, y, t, h, o, n
