3927a38d9a1d211dfbe3251b51586d336994a8fa
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-25 13:28:23 +0100

    python data type

---
+++ Python tutorials for beginners/data_type.py
@@ -0,0 +1,5 @@
+x = 100                       # x is integer
+pi = 3.14                     # pi is float
+empname = "python is great"   # empname is string
+
+a = b = c = 100 # this statement assign 100 to c, b and a.
