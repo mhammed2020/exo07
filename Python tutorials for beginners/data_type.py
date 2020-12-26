6bd26579fa4dbc6761abd31545fdbd69e14e2299
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-26 18:54:47 +0100

    break statement

--- Python tutorials for beginners/data_type.py
+++ Python tutorials for beginners/data_type.py
@@ -367,3 +367,17 @@ count = 0
 while count < 10:
     print(count)
     count += 1
+
+
+
+
+count = 0
+
+while count < 10:
+    count += 1
+    if count == 5:
+         break    
+    print("inside loop", count)
+
+
+print("out of while loop")
