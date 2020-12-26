c10d7f065647af19884ee213aba26680e830d9e7
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-26 18:50:29 +0100

    Keyword arguments

--- Python tutorials for beginners/data_type.py
+++ Python tutorials for beginners/data_type.py
@@ -318,3 +318,14 @@ def func():
     print(global_var)   # you can access global variables in side function
 
 func()                  # calling function func()
+
+
+
+def named_args(name, greeting):
+    print(greeting + " " + name )
+
+
+
+named_args(name='jim', greeting='Hello')
+
+named_args(greeting='Hello', name='jim') # you can pass arguments this way too
