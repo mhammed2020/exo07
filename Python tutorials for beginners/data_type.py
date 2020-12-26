98f4e38aad56fa098647b496d938ede6362c329d
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-26 18:47:09 +0100

    Function with return value

--- Python tutorials for beginners/data_type.py
+++ Python tutorials for beginners/data_type.py
@@ -287,3 +287,13 @@ def sum(start, end):
     print(result)
 
 sum(10, 50)
+
+
+def sum(start, end):
+       result = 0
+   for i in range(start, end + 1):
+       result += i
+   return result
+
+s = sum(10, 50)
+print(s)
