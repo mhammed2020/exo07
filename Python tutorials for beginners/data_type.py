fbbb3d4f94258597c3c8ba0aa10c4e509cd58745
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-28 10:54:00 +0100

    # file my_module.py

--- Python tutorials for beginners/data_type.py
+++ Python tutorials for beginners/data_type.py
@@ -944,3 +944,18 @@ def fact(n):
 
 print(fact(0))
 print(fact(5))
+
+
+# What is if __name__ == "__main__" ??
+
+# file my_module.py
+
+foo = 100
+
+def hello():
+    print("i am from my_module.py")
+
+if __name__ == "__main__":
+    print("Executing as main program")
+    print("Value of __name__ is: ", __name__)
+    hello()
