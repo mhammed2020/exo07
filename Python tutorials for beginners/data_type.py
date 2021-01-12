a5477f3baa38436393a62919ad389b24fdaa660d
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-01-12 17:52:21 +0100

    everything is an object in python

--- Python tutorials for beginners/data_type.py
+++ Python tutorials for beginners/data_type.py
@@ -1015,3 +1015,22 @@ print(num,type(num))
 
 for i in num:
     print(i)
+
+num1 = 3
+print(num1)
+print(id(num1))
+print(type(num1))
+print(id(3))
+num2 = 3
+print(num2)
+print(id(num2))
+print(type(num2))
+
+print(num1 == num2)
+print(num1 is num2)
+
+num2 = (3,)
+print(type(num2))
+print(id(num2))
+print(num1 == num2)
+print(num1 is num2)
