827273f71ee198b53e5693e40c5697bd3df1c663
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-01-12 17:48:52 +0100

    test boolean example

--- Python tutorials for beginners/data_type.py
+++ Python tutorials for beginners/data_type.py
@@ -984,3 +984,18 @@ d = {
 }
 
 "Sam had {hats} hats and {mats} mats".format(**d)
+
+
+
+#====================================================================
+num1 = 3
+num2 = 5
+
+print(num1==num2)
+print(num1< num2)
+print(num1 > num2)
+
+num1 = True
+num2 = False
+print(type(num1) )
+print( type(num2))
