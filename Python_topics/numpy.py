d5947ec7e321f512813f7e795adb94a8ce200689
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-01-13 19:27:40 +0100

    Slicing in the Array

--- Python_topics/numpy.py
+++ Python_topics/numpy.py
@@ -11,3 +11,10 @@ import numpy as np
 a = np.array([[1,2,3,4,5,6,7]])  
 print("Array Size:",a.size)  
 print("Shape:",a.shape)  
+
+
+
+import numpy as np  
+a = np.array([[1,2],[3,4],[5,6]])  
+print(a[0,1])  
+print(a[2,0])  
