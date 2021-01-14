5225872a6a466a270043cc4836a813971a8f7343
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-01-14 22:25:32 +0100

    function with positional arguments

--- Python_topics/greeter.py
+++ Python_topics/greeter.py
@@ -8,3 +8,10 @@ def greet_user(username):
     """Display a simple greeting."""
     print(f"Hello, {username.title()}!")
 greet_user('jesse')
+
+
+def describe_pet(animal_type, pet_name):
+    """Display information about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+describe_pet('hamster', 'harry')
