f1a19a0501f759f2424b7ef2ea5569d64269fca6
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-01-14 22:24:20 +0100

    Passing Information to a Function

--- Python_topics/greeter.py
+++ Python_topics/greeter.py
@@ -2,3 +2,9 @@ def greet_user():
     """Display a simple greeting."""
     print("Hello!")
 greet_user()
+
+
+def greet_user(username):
+    """Display a simple greeting."""
+    print(f"Hello, {username.title()}!")
+greet_user('jesse')
