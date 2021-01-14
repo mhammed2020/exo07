923a3ee8b65a6ee2bf957010fc728369a53f59f8
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-01-14 22:21:36 +0100

    Removing All Instances of Specific Values from a List

--- Python_topics/inputfunction.py
+++ Python_topics/inputfunction.py
@@ -1,2 +1,10 @@
 message = input("Tell me something, and I will repeat it back to you: ")
 print(message)
+
+
+
+pets = ['dog', 'cat', 'dog', 'goldfish', 'cat', 'rabbit', 'cat']
+print(pets)
+while 'cat' in pets:
+    pets.remove('cat')
+print(pets)
