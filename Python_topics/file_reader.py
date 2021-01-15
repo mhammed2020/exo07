52c937e88b5df7f8c3954fa2ccf1def4075ca4aa
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-01-15 20:09:48 +0100

    read File Paths assigned to a variable

--- Python_topics/file_reader.py
+++ Python_topics/file_reader.py
@@ -8,7 +8,12 @@ with open('pi_digits.txt') as file_object:
     print(contents.rstrip())
 
 
-    
+
+#----------
+
+file_path = '/home/ehmatthes/other_files/text_files/filename.txt'
+with open(file_path) as file_object:
+    pass
     
 
 
