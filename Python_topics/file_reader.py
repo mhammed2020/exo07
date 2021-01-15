33e246ef31479a6ee7552da86bfea3142ad223c6
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-01-15 20:07:41 +0100

    remove the extra blank line with rstrip()

--- Python_topics/file_reader.py
+++ Python_topics/file_reader.py
@@ -1,3 +1,15 @@
 with open('pi_digits.txt') as file_object:
     contents = file_object.read()
 print(contents)
+
+
+with open('pi_digits.txt') as file_object:
+    contents = file_object.read()
+    print(contents.rstrip())
+
+
+    
+    
+
+
+    
