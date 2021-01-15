9de9e5ecb0f88681e9e90a5ddccc98fa2bd32ea1
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-01-15 20:13:07 +0100

    Making a List of Lines from a File

--- Python_topics/file_reader2.py
+++ Python_topics/file_reader2.py
@@ -2,3 +2,13 @@ filename = 'pi_digits.txt'
 with open(filename) as file_object:
     for line in file_object:
         print(line)
+
+
+#------------------------------------
+
+
+filename = 'pi_digits.txt'
+with open(filename) as file_object:
+    lines = file_object.readlines()
+for line in lines:
+    print(line.rstrip())
