e859f1ea98dcdc7ec1ba89e336127dd3e250dd9d
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-01-19 22:59:54 +0100

    Using a Colormap

--- Python_topics/projects/mpl_squares.py
+++ Python_topics/projects/mpl_squares.py
@@ -16,4 +16,7 @@ x_values = [1, 2, 3, 4, 5]
 y_values = [1, 4, 9, 16, 25]
 ax.scatter(x_values, y_values, c='red', s=10)
 ax.scatter(x_values, y_values, c=(0, 0.8, 0), s=10)
+
+
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 plt.show()
