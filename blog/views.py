6be64d648de9904c2b2ceebe144dee844d37e7d3
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-01-27 21:18:19 +0100

    HomepageView using  cbvs

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Post
+
+# class-based view
+
+class HomepageView(ListView):
+    model = Post
+    paginate_by = 5
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
