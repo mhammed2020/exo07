c53f8385e0e0a8e4cf97c70c71f86f8e67ea3c69
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-01-27 21:22:04 +0100

    function-based view

--- blog/views.py
+++ blog/views.py
@@ -10,3 +10,17 @@ class HomepageView(ListView):
     paginate_by = 5
     template_name = 'blog/index.html'
     context_object_name = 'posts'
+    
+    
+from django.core.paginator import Paginator
+
+def function_based(request):
+
+    posts = Post.objects.all()
+    paginator = Paginator(posts, 5)
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
+
+    return render(request,
+                  'blog/function_based.html',
+                  {'posts': posts})
